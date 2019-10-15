d27b9eae45c1a05ea4a3e6d8d7f2cd1b1196198b
torianne02 <victoria.fluharty@gmail.com>
2019-10-15 15:56:16 -0700

    adds password in Guesser validation check adds has_secure_password to model

--- app/models/guesser.rb
+++ app/models/guesser.rb
@@ -1,5 +1,6 @@
 class Guesser < ActiveRecord::Base
+  has_secure_password 
   has_many :secret_keepers
 
-  validates :initials, presence: true
+  validates :initials, :password, presence: true
 end 
