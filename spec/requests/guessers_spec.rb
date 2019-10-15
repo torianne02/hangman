cf44d639d97252370a648ca98ba1064a7dbb3712
torianne02 <victoria.fluharty@gmail.com>
2019-10-15 14:37:44 -0700

    creates guessers_spec.rb within requests folder adds test logic for guesser signup

---
+++ spec/requests/guessers_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe GuessersController, type: :request do 
+  # initialize test data for sign-up/sign-in
+  let(:guesser) { create(:guesser) }
+  let(:guesser_id) { guesser.id }
+  let(:guesser_valid_attr) do
+    attributes_for(:guesser)
+  end 
+
+  # test suite for guesser sign-up
+  describe 'POST /signup' do
+    context 'when valid request' do
+      before { post '/signup', params: guesser_valid_attr.to_json }
+
+      it 'creates a new guesser' do 
+        expect(response).to have_http_status(201)
+      end
+
+      it 'returns success message' do
+        expect(json['message']).to match(/Account created successfully/)
+      end
+    end
+  end 
+end 
