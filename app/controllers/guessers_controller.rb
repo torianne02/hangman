96421bf5f81f52bfe0cae6d83c858470e5bb3aff
torianne02 <victoria.fluharty@gmail.com>
2019-10-15 15:31:22 -0700

    adds logic for POST /signup

--- app/controllers/guessers_controller.rb
+++ app/controllers/guessers_controller.rb
@@ -1,6 +1,19 @@
 class GuessersController < ApplicationController
+  # POST /signup
+  def create 
+    # binding.pry
+    @guesser = Guesser.create!(guesser_params)
+
+    response = { message: Message.account_created }
+
+    json_response(response, :created)
+  end
 
 
+  private
 
+  def guesser_params 
+    params.permit(:initials)
+  end 
 
 end 
