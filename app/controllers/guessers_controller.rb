dfb2e30ba0072f4b8add725f1448d3006043f99f
torianne02 <victoria.fluharty@gmail.com>
2019-10-15 18:46:59 -0700

    creates show method in Guessers controller adds private method set_guesser sets before_action method

--- app/controllers/guessers_controller.rb
+++ app/controllers/guessers_controller.rb
@@ -1,4 +1,6 @@
 class GuessersController < ApplicationController
+  before_action :set_guesser, only: [:show, :update, :destory]
+
   # POST /signup
   def create 
     @guesser = Guesser.create!(guesser_params)
@@ -6,10 +8,18 @@ class GuessersController < ApplicationController
     json_response(response, :created)
   end
 
+  # GET /guessers/:id
+  def show 
+    json_response(@guesser)
+  end 
 
   private
 
   def guesser_params 
     params.permit(:initials, :password)
+  end 
+
+  def set_guesser 
+    @guesser = Guesser.find(params[:id])
   end 
 end 
