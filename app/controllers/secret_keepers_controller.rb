bf0c2f3b90c2ffd894ed8189fc502e442f07261b
torianne02 <victoria.fluharty@gmail.com>
2019-10-16 14:18:06 -0700

    adds secret_keepers#create method creates private methods and before_action

--- app/controllers/secret_keepers_controller.rb
+++ app/controllers/secret_keepers_controller.rb
@@ -1,3 +1,19 @@
 class SecretKeepersController < ApplicationController
+  before_action :set_guesser
   
-end 
+  # POST /guessers/:id/
+  def create 
+    @guesser.secret_keepers.create!(secret_keeper_params)
+    json_response(@guesser, :created)
+  end
+
+  private
+
+  def secret_keeper_params
+    params.permit(:word, :guesser_id, :guesses)
+  end
+
+  def set_guesser
+    @guesser = Guesser.find(params[:guesser_id])
+  end 
+end
