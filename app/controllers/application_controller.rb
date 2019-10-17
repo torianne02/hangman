c0127f0a63a5b674f2357558021a1a669316d8cd
torianne02 <victoria.fluharty@gmail.com>
2019-10-17 16:01:30 -0700

    adds sessions functionality to other controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :current_guesser, except: [:home]
+  helper_method :current_guesser 
   include Response
   include ExceptionHandler
+
+  def logged_in?
+    !!current_guesser
+  end 
+
+  private
+
+  def current_guesser
+    @current_guesser ||= Guesser.find(session[:guesser_id]) if session[:guesser_id]
+  end 
 end
--- app/controllers/guessers_controller.rb
+++ app/controllers/guessers_controller.rb
@@ -1,10 +1,12 @@
 class GuessersController < ApplicationController
   before_action :set_guesser, only: [:show, :update, :destroy]
+  before_action :current_guesser, except: [:new, :create]
 
   # POST /signup
   def create 
     @guesser = Guesser.create!(guesser_params)
     response = { message: Message.account_created }
+    session[:guesser_id] = @guesser.id
     json_response(response, :created)
   end
 
