3aa9f7dd046a92570861a94562e1bc95371f9a82
torianne02 <victoria.fluharty@gmail.com>
2019-10-18 12:31:27 -0700

    updates signup routes to guessers#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+  get '/signup', to: 'guessers#new'
   post '/signup', to: 'guessers#create'
   get '/signin', to: 'sessions#new'
   get '/logout', to: 'sessions#destroy'
