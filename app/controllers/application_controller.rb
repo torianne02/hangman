719ebce443d5e2d0329fdaf0d14567883536b5e8
torianne02 <victoria.fluharty@gmail.com>
2019-10-18 10:17:56 -0700

    adds protect_from_forgery method and require_logged_in method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   before_action :current_guesser, except: [:home]
+  before_action :require_logged_in, except: %i[new create home]
   helper_method :current_guesser 
+
   include Response
   include ExceptionHandler
 
@@ -9,6 +12,10 @@ class ApplicationController < ActionController::Base
   end 
 
   private
+
+  def require_logged_in
+    redirect_to root_path unless logged_in?
+  end 
 
   def current_guesser
     @current_guesser ||= Guesser.find(session[:guesser_id]) if session[:guesser_id]
